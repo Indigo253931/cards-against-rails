e8ae87fa0ddf9a63046f865f73f47fcc1ff63825
J.P. Barela <barela@jpbarela.com>
2016-08-22 08:49:42 -0600

    Cleanup api routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	root to: 'home#index' 
 
-	namespace :api, except: [:new, :edit], defaults: { format: 'json' } do
+	namespace :api, except: [:new, :edit] do
 		resources :cards, except: [:edit, :delete]
 	end
 end
