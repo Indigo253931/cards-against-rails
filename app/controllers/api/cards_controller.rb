9cb1acfa96259e581b9a0c6eb2b24c43b0564436
J.P. Barela <barela@jpbarela.com>
2016-08-21 22:34:04 -0600

    Add cards api.

---
+++ app/controllers/api/cards_controller.rb
@@ -0,0 +1,34 @@
+module Api
+	class CardsController < ActionController::Base
+		
+		class << self 
+			attr_accessor :cards, :next_card_id
+		end
+
+		self.cards = [
+			{question: 'Where do the files for ActiveSuck classes go?' , id: 1}, 
+			{question: "How come there's a skynet gem dependency in project?", id: 2},
+			{question: '_____ changed from development to production?', id: 3},
+			{question: "Why isn't hogwarts a good name for the base application class?", id: 4},
+			{question: "rails g _______ ", id: 5}
+		]
+
+		self.next_card_id = 6
+
+		def index 
+			render json: CardsController.cards
+		end
+
+		def show
+			card = CardsController.cards.select { |card| card[:id] == params[:id].to_i }
+			render json: card 
+		end
+
+		def create
+			new_card = {question: params[:question], id: CardsController.next_card_id}
+			CardsController.next_card_id += 1
+			CardsController.cards.push new_card
+			render json: new_card
+		end
+	end
+end 
